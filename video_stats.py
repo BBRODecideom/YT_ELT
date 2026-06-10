c283406dfb5efb6b44334ff8a6397ba00c579c36
Benoît Brossillon <bbrossillon@decideom.fr>
2026-06-10 15:37:15 +0200

    Ajout de la recuperation des videos

--- video_stats.py
+++ video_stats.py
@@ -11,6 +11,7 @@ load_dotenv(BASE_DIR / ".env")
 
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "bendu78130new"
+maxResults = 10
 
 
 def get_playlist_id():
@@ -42,7 +43,51 @@ def get_playlist_id():
     
     except requests.exceptions.RequestException as e:
         raise e
+    
+
+def get_video_ids(playlistId):
+
+    video_ids=[]
+
+    pageToken = None 
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
+
+    try:
+
+        while True:
+            
+            url = base_url
+
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+            
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []):
+            
+                video_id = item['contentDetails']['videoId']          
+                video_ids.append(video_id)
+            
+
+            if 'nextPageToken' in data:
+                pageToken = data['nextPageToken']
+            else:
+                break
+
+            return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
 
 if __name__ == "__main__":
 
-    get_playlist_id()
+    playlistId = get_playlist_id()
+    video_ids = get_video_ids(playlistId)
+    print(video_ids)
+
