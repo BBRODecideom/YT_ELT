6d9c1cdb405c9281e78175faee83a4923186abf8
Benoît Brossillon <bbrossillon@decideom.fr>
2026-06-10 14:35:30 +0200

    add venv to gitignore ; add api key on env

---
+++ video_stats.py
@@ -0,0 +1,48 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent
+load_dotenv(BASE_DIR / ".env")
+
+
+API_KEY = os.getenv("API_KEY")
+CHANNEL_HANDLE = "bendu78130new"
+
+
+def get_playlist_id():
+
+    try:
+
+        url = f"https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
+
+
+        response = requests.get(url)
+
+        response.raise_for_status()
+
+        #print(response) 
+
+        data = response.json() 
+
+        #print(data)
+
+        ##method use to convert url to json and 4 is a convention
+        #print(json.dumps(data, indent=4))
+        ##id = channel id. uploads = playlist id
+
+        channel_items = data['items'][0]
+        channel_playlist_id = channel_items['contentDetails']['relatedPlaylists']['uploads']
+
+        print(channel_playlist_id) 
+        return channel_playlist_id
+    
+    except requests.exceptions.RequestException as e:
+        raise e
+
+if __name__ == "__main__":
+
+    get_playlist_id()
