9e77fd6197c0331beebc75361b44143090ba68c6
Benoît Brossillon <bbrossillon@decideom.fr>
2026-06-14 11:27:48 +0200

    Adding json file to data folder and prepare gitignore with logs folder

--- video_stats.py
+++ video_stats.py
@@ -3,6 +3,7 @@ import json
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+from datetime import date
 
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -130,12 +131,19 @@ def extract_video_data(video_id):
     except requests.exceptions.RequestException as e:
         raise e
 
+def save_to_json(extracted_data):
 
+    file_path = f"./data/YT_Data_{date.today()}.json"
+
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
+        
 
 
 if __name__ == "__main__":
 
     playlistId = get_playlist_id()
     video_ids = get_video_ids(playlistId)
-    extract_video_data(video_ids)
-    
+    video_data = extract_video_data(video_ids)
+    save_to_json(video_data)
+
